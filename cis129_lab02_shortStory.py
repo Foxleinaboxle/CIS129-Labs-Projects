f7df50cba87fd6cc1ec7dfb920a6f486e28158c8
Foxleinaboxle <113534555+Foxleinaboxle@users.noreply.github.com>
2024-09-13 22:42:22 -0700

    Update cis129_lab02_shortStory.py
    Removed "Hello, World!"
    Replaced with shot story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print("Hello, World!")
+#Author: Vixen Hinkle
+#The first codeing lab cor cis129
+#prints a story about a knight and a wolf
+print("Once upon a time, a knight came across a massive wolf. ")
+print("Upon seening that the wolf was asleep the knight atempted to sneak past.")
+print("Unfortunatly the knights large and cluncky armor awoke the wolf.")
+print("The kight made an effort to deefend himself from his impending doom. ")
+print("However it was all for naught as the wolf gobbled up the knight all in one gulp. ")
+print("Before returning to sleep upon its ever growing pile of armor.")
